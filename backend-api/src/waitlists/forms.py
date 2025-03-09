2a75c5b81205bb8a395df3d6a0fff3144d095be2
koala86 <nanbiao66@gmail.com>
2025-03-10 05:59:54 +0900

    first commit

---
+++ backend-api/src/waitlists/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.utils import timezone
+
+from .models import WaitlistEntry
+
+
+class WaitlistEntryCreateForm(forms.ModelForm):
+    class Meta:
+        model = WaitlistEntry
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        today = timezone.now().day
+        qs = WaitlistEntry.objects.filter(email=email, timestamp__day=today)
+        if qs.count() >= 5:
+            raise forms.ValidationError("Can not enter this email again today")
+        return email
